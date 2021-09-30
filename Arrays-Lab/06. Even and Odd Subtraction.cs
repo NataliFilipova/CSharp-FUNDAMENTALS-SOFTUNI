d58d5b72e96456783c9804097e1ecad5ed940d0f
Natali Filipova <56434467+NataliFilipova@users.noreply.github.com>
2021-09-30 15:53:02 +0300

    Add files via upload

---
+++ Arrays-Lab/06. Even and Odd Subtraction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+
+            int oddSum = 0;
+            int evensum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+                if (currentNumber % 2 == 0)
+                {
+                    evensum += currentNumber;
+                }
+                else
+                {
+                    oddSum += currentNumber;
+                }
+            }
+            int diffference = evensum - oddSum;
+            Console.WriteLine(diffference);
+        }
+    }
+}
