8490f932916c3ff979d6e152922fb662be13ad64
Natali Filipova <56434467+NataliFilipova@users.noreply.github.com>
2021-10-01 19:08:18 +0300

    Add files via upload

---
+++ Exercise: Arrays/02. Common Elements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrOne = Console.ReadLine().Split(" ");
+            string[] arrTwo = Console.ReadLine().Split(" ");
+
+            foreach(string elementsTwo in arrTwo)
+            {
+                for(int i = 0; i <arrOne.Length; i++)
+                {
+                    string elementOne = arrOne[i];
+                    if(elementsTwo == elementOne)
+                    {
+                        Console.Write(elementOne + (" "));
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
